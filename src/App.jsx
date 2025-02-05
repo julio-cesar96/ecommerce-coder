990f7529a9d28b5ce5c8a3c96e845a0789f044b7
julio-cesar96 <julio_cbs96@hotmail.com>
2025-02-05 12:04:19 -0300

    aula sobre o hook useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { useRef, useEffect } from "react";
+
 import "./App.css";
 
 
 function App() {
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current?.focus();
+    }
+  }, []);
 
   return (
    <div>
-    
+      <h2> useRef para Manipular a DOM</h2>
+      <input ref={inputRef} type="text" placeholder="Digete alguma coisa..." />
    </div>
   )
 }
