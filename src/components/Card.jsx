46c3e418ca1e66c8bb212cbef3fc2543928e9d41
julio-cesar96 <julio_cbs96@hotmail.com>
2025-02-05 10:34:24 -0300

    aula sobre jsx

---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+import './Card.styles.css'
+
+const Card = () => {
+
+    return (
+        <div style={{
+            backgroundColor: 'blue',
+            width: '400px',
+            height: '400px',
+        }}>
+            Conteudo do meu cartão
+        </div>
+    )
+}
+
+export default Card;
