e12992f202f3d4b13c71f370abab5497982f4c00
julio-cesar96 <julio_cbs96@hotmail.com>
2025-02-05 21:42:00 -0300

    app principal

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import NotFound from "./components/NotFound/NotFound"
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
  
 
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
